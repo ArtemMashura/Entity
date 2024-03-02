55b2d026f00e38cdcc3ef8b4bc3c2eda59b19d09
ArtemMashura <Masu_ih91@student.itstep.org>
2024-03-02 02:24:02 +0200

    Добавьте файлы проекта.

---
+++ Entity/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using Entity.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private EntityStartContext _context;
+        public ValuesController(EntityStartContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<user> users = await _context.users.ToListAsync();
+            if (users == null)
+            {
+                NotFound();
+            }
+
+            return Ok(users);
+        }
+    }
+}
